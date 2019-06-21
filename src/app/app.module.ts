e54d208200de406465999197ae7c1eca61500bb8
Rakiir <10715815+Rakiir@users.noreply.github.com>
2019-06-21 12:46:01 -0400

    Update app.module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -30,10 +30,3 @@ import { CartService } from './cart.service';
 })
 
 export class AppModule { }
-
-
-/*
-Copyright Google LLC. All Rights Reserved.
-Use of this source code is governed by an MIT-style license that
-can be found in the LICENSE file at http://angular.io/license
-*/
